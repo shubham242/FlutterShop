d5fc8f32114ae0cad6229f7b3bd3daf0e9434337
Shubham <vivekgoyal164@gmail.com>
2021-08-05 04:01:06 +0530

    Http Reqs

--- lib/screens/orders.dart
+++ lib/screens/orders.dart
@@ -5,43 +5,32 @@ import '../providers/orders.dart';
 import '../widgets/order_item.dart';
 import '../widgets/my_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoad = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoad = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchOrders();
-      setState(() {
-        _isLoad = false;
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Orders'),
-      ),
-      drawer: MyDrawer(),
-      body: _isLoad
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: ordersData.orders.length,
-              itemBuilder: (ctx, i) => OrdItem(ordersData.orders[i])),
-    );
+        appBar: AppBar(
+          title: Text('Orders'),
+        ),
+        drawer: MyDrawer(),
+        body: FutureBuilder(
+          future: Provider.of<Orders>(context, listen: false).fetchOrders(),
+          builder: (ctx, dataSS) {
+            if (dataSS.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              if (dataSS.error == null) {
+                return Consumer<Orders>(
+                    builder: (ctx, ordersData, child) => ListView.builder(
+                        itemCount: ordersData.orders.length,
+                        itemBuilder: (ctx, i) =>
+                            OrdItem(ordersData.orders[i])));
+              }
+            }
+            return Container();
+          },
+        ));
   }
 }
